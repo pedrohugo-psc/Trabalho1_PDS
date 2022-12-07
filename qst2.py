fa014fd20d7e1ad0f818370a088536d2ee46e43e
pedrohugo-psc <pedro.hugo@ufba.br>
2022-12-07 08:36:09 -0300

    Finalizando a programacao do trabalho 1

--- qst2.py
+++ qst2.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 
 fs = 40 # frequência de amostragem
 ps = 1/fs # período de amostragem
-n = np.arange(0,40,ps) # 1600 amostras
+n = np.arange(0,40,ps)
+n1 = np.arange(0,1,ps) # 40 amostras
+n1_1 = np.pad(n1, (0,len(n) - len(n1)),'constant')
 t = n*ps # tempo
-print(n)
+M = 60
 
-x1 = np.cos(0.2*np.pi*n) # x1 = np.cos(0.1*2*np.pi*n) 
-x2 = np.cos(0.4*np.pi*n - np.pi/2) # x2 = np.cos(0.2*2*np.pi*n + np.pi/2)
-x3 = np.cos(0.8*np.pi*n + np.pi/5) # x3 = np.cos(0.4*2*np.pi*n + np.pi/5)
+wn =  0.54 - 0.46 * np.cos((2*np.pi*n1_1)/M)
+
+x1 = wn * np.cos(0.2*np.pi*(n-M-1))
+x2 = wn * np.cos(0.4*np.pi*(n - 2*M - 2) - np.pi/2)
+x3 = wn * np.cos(0.8*np.pi*n + np.pi/5)
 
 x = x1 + x2 + x3
 
@@ -45,12 +49,15 @@ plt.plot(freq,np.angle(fft_transformada))
 fs_2 = 20000 # frequência de amostragem
 ps_2 = 1/fs_2 # período de amostragem
 n_2 = np.arange(0,40,ps_2) # 800000 amostras
+n_2_1 = np.arange(0,1/500,ps_2) # 40 amostras
+n_2_1_1 = np.pad(n1, (0,len(n_2) - len(n_2_1)),'constant')
 t_2 = n_2 * ps_2 # tempo
-print(len(n_2))
 
-x1 = np.cos(0.2*np.pi*n_2)
-x2 = np.cos(0.4*np.pi*n_2 - np.pi/2)
-x3 = np.cos(0.8*np.pi*n_2 + np.pi/5)
+wn =  0.54 - 0.46 * np.cos((2*np.pi*n_2_1_1)/M)
+
+x1 = wn * np.cos(0.2*np.pi*(n_2-M-1))
+x2 = wn * np.cos(0.4*np.pi*(n_2 - 2*M - 2) - np.pi/2)
+x3 = wn * np.cos(0.8*np.pi*n_2 + np.pi/5)
 
 x = x1 + x2 + x3
 
@@ -63,15 +70,11 @@ plt.plot(t_2,x)
 fft_transformada = np.fft.fft(x)
 freq = np.fft.fftfreq(len(fft_transformada),ps_2)
 
-fft_mag = 2*np.abs(fft_transformada/len(n_2))
-
 plt.figure(6)
 plt.title("Sinal da Transfomada de Fourier de x[n] II")
 plt.ylabel("Magnitude")
 plt.xlabel("Frequência (Hz)")
 plt.plot(freq[freq > 0],np.abs(fft_transformada)[freq > 0])
 
+
 plt.show()
-
-
-
